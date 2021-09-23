bd751117e919837914aad3f6e1042ad836a24730
TaewooKim <embedded61@gmail.com>
2021-09-23 22:30:35 +0900

    Add: production 환경에서 구동할 수 있도록 환경 설정 및 dockerfile 생성

---
+++ moranique/settings/prod.py
@@ -0,0 +1,30 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_env_variable('SQL_DATABASE'),
+        'USER': get_env_variable('SQL_USER'),
+        'PASSWORD': get_env_variable('SQL_PASSWORD'),
+        'HOST': get_env_variable('SQL_HOST'),
+        'PORT': get_env_variable('SQL_PORT'),
+    }
+}
+
+DEBUG = False
+
+
+AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = get_env_variable('AWS_REGION') 
+
+###S3 Storages
+AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME') 
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
